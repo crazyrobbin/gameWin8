6184df61575d1f1268ed159b607824a12cd5b853
Robbin Singh <crazyrobbin@gmail.com>
2015-01-08 10:44:13 -0800

    initial commit

---
+++ Game/MainPage.xaml.cs
@@ -0,0 +1,165 @@
+﻿using Game.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Net.Http;
+using Windows.Data.Json;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace Game
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    /// 
+
+    public sealed partial class MainPage : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        /// <summary>
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation. Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session. The state will be null the first time a page is visited.</param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+            if (e.PageState != null && e.PageState.ContainsKey("username"))
+            {
+                loginError.Text = "LogedIn";
+            }
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+            e.PageState["username"] = Global.username;
+            e.PageState["name"] = Global.name;
+        }
+
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// 
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// and <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        async private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            loginError.Text = "";
+            Global.username = usernameLogin.Text;
+            if (Global.username == "")                             //check username
+            {
+                loginError.Text = "Enter Username";
+                return;
+            }
+            string password = passwordLogin.Password;       //check password
+            if (password == "")
+            {
+                loginError.Text = "Enter Password";
+                return;
+            }
+
+            ProgressRingLogin.IsActive = true;
+            try
+            {
+                var client = new HttpClient(); // Add: using System.Net.Http;
+                var response = await client.GetAsync(new Uri("http://localhost/test/one/login/"+Global.username));
+                var result = await response.Content.ReadAsStringAsync();
+                ProgressRingLogin.IsActive = false;
+                JsonValue jsonValue = JsonValue.Parse(result);
+
+                if (jsonValue.GetObject().GetNamedString("result") == "true")
+                {
+                    loginError.Text = "Welcome";
+                    Global.name = jsonValue.GetObject().GetNamedString("name");
+                    if (this.Frame != null)
+                    {
+                        this.Frame.Navigate(typeof(Home));
+                    }
+                }
+                else
+                    loginError.Text = "Invalid username or password, Register First";
+            }
+            catch
+            {
+                ProgressRingLogin.IsActive = false;
+                loginError.Text = "No Internet Connection";
+            }
+        }
+
+        private void usernameLogin_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            usernameLogin.Focus(FocusState.Programmatic);
+        }
+    }
+}
